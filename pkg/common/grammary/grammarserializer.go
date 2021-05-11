48fd863ed2b8fb0c6fa6c302059c06a3579ab4ad
UsingCoding <extendedmoment@outlook.com>
2021-05-11 15:53:43 +0300

    Поправил сериализатор грамматики, чтобы аксиома была первым правилом при сериализации

--- pkg/common/grammary/grammarserializer.go
+++ pkg/common/grammary/grammarserializer.go
@@ -27,17 +27,30 @@ func (serializer Serializer) SerializeGrammar(grammar interface{}) (string, erro
 
 func serialize(grammar *Grammar) string {
 	var buffer string
+
+	axiomPtr := grammar.Axiom
+	buffer += serializeRoll(*axiomPtr, grammar.Impl[*axiomPtr])
+
 	for symbol, alternativesRolls := range grammar.Impl {
-		for _, roll := range alternativesRolls {
-			rule := symbol.ch + " " + RuleSidesSeparator + " "
-			for _, symbolInRule := range roll {
-				rule += symbolInRule.ch + " "
-			}
-			rule = strings.Trim(rule, " ")
-			buffer += rule + "\n"
+		if symbol == *axiomPtr {
+			continue
 		}
+		buffer += serializeRoll(symbol, alternativesRolls)
 	}
 	return strings.Trim(buffer, "\n")
+}
+
+func serializeRoll(leftSideSymbol Symbol, rolls [][]Symbol) string {
+	var buffer string
+	for _, roll := range rolls {
+		rule := leftSideSymbol.ch + " " + RuleSidesSeparator + " "
+		for _, symbolInRule := range roll {
+			rule += symbolInRule.ch + " "
+		}
+		rule = strings.Trim(rule, " ")
+		buffer += rule + "\n"
+	}
+	return buffer
 }
 
 func serializeWithSequences(grammar *GrammarWithHeadSequences) string {
