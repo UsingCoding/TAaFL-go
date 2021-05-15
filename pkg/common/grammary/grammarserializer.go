7fbc65a1e50063b425faf1e39394d12db0f81311
UsingCoding <extendedmoment@outlook.com>
2021-05-15 19:48:52 +0300

    Поправил сериализатор грамматики

--- pkg/common/grammary/grammarserializer.go
+++ pkg/common/grammary/grammarserializer.go
@@ -18,11 +18,10 @@ func (serializer Serializer) SerializeGrammar(grammar interface{}) (string, erro
 	case *Grammar:
 		return serialize(grammar), nil
 	case *GrammarWithHeadSequences:
-		return serializeWithSequences(grammar), nil
+		return serializeWithSequenceBeta(grammar), nil
 	default:
 		return "", errors.New(fmt.Sprintf("unknown grammar type %T", grammar))
 	}
-
 }
 
 func serialize(grammar *Grammar) string {
@@ -53,19 +52,31 @@ func serializeRoll(leftSideSymbol Symbol, rolls [][]Symbol) string {
 	return buffer
 }
 
-func serializeWithSequences(grammar *GrammarWithHeadSequences) string {
+func serializeWithSequenceBeta(grammar *GrammarWithHeadSequences) string {
 	var buffer string
-	for symbol, alternativesRolls := range grammar.Grammar.Impl {
-		for rollNumber, roll := range alternativesRolls {
-			rule := symbol.ch + " " + RuleSidesSeparator + " "
-			for _, symbolInRule := range roll {
-				rule += symbolInRule.ch + " "
-			}
 
-			rule += RuleSequenceSeparator + " "
-			rule += strings.Join(grammar.Sequences[symbol][rollNumber], " ")
-			buffer += rule + "\n"
+	axiomPtr := grammar.Grammar.Axiom
+	buffer += serializeRollWithHeadSequence(*axiomPtr, grammar.Grammar.Impl[*axiomPtr], grammar.Sequences[*axiomPtr])
+
+	for symbol, alternativesRolls := range grammar.Grammar.Impl {
+		if symbol == *axiomPtr {
+			continue
 		}
+		buffer += serializeRollWithHeadSequence(symbol, alternativesRolls, grammar.Sequences[symbol])
+	}
+	return strings.Trim(buffer, "\n")
+}
+
+func serializeRollWithHeadSequence(leftSideSymbol Symbol, rolls [][]Symbol, sequence [][]string) string {
+	var buffer string
+	for rollNumber, roll := range rolls {
+		rule := leftSideSymbol.ch + " " + RuleSidesSeparator + " "
+		for _, symbolInRule := range roll {
+			rule += symbolInRule.ch + " "
+		}
+		rule += RuleSequenceSeparator + " "
+		rule += strings.Join(sequence[rollNumber], " ")
+		buffer += rule + "\n"
 	}
 	return buffer
 }
