93307c3a0d3c4569562710f137cd93c88c63a35a
UsingCoding <extendedmoment@outlook.com>
2021-05-15 18:19:07 +0300

    Поправил код генератора таблиц и бегунка(для трассировки при ошибке). Поправил пример грамматики и входной программы

--- pkg/generator/app/generator.go
+++ pkg/generator/app/generator.go
@@ -16,70 +16,98 @@ symbol | NM | ptr to left part of rule or next string | shift(0/1) | stack(0/1)
 func CreateTables(data string) ([][]string, [][]string) {
 	m := make(map[string]int)
 	var leftParts, rightParts [][]string
+
 	var ptrToRightPart int
+
 	for _, line := range strings.Split(data, "\n") {
+
 		elem := ""
-		creatingNM, rightPartFlag := false, false
+		creatingNM, leftPartFlag := false, false
 		left, right := []string{}, [][]string{}
+
 		for _, i := range line {
 			ch := string(i)
+
 			if ch != " " {
 				elem += ch
 				continue
 			}
+
 			if elem == grammary.RuleSidesSeparator {
 				elem = ""
 				continue
 			}
+
 			if elem == grammary.RuleSequenceSeparator {
 				creatingNM = true
 				elem = ""
 				continue
 			}
+
 			if elem == grammary.EmptySymbol {
+
 				temp := []string{}
 				temp = append(temp, elem)
+
 				right = append(right, temp)
+
 				elem = ""
 				continue
 			}
+
 			if !creatingNM {
+
 				if grammary.IsNonTerminalSymbol(elem) {
-					if strings.HasPrefix(line, elem) && !rightPartFlag {
-						v, ok := m[elem]
-						v = 0 + v
+
+					if strings.HasPrefix(line, elem) && !leftPartFlag {
+
+						_, ok := m[elem]
+
 						//add nonterminal in map with number of his string
 						if !ok {
 							m[elem] = len(leftParts) + len(left)
 						}
+
 						left = append(left, elem)
-						rightPartFlag = true
+						leftPartFlag = true
+
 					} else {
+
 						temp := []string{}
 						temp = append(temp, elem)
+
 						right = append(right, temp)
 					}
+
 					elem = ""
+
 				} else {
+
 					temp := []string{}
 					temp = append(temp, elem)
 					temp = append(temp, elem) // add NM to terminal symbol
+
 					right = append(right, temp)
+
 					elem = ""
 				}
 			}
 		}
+
 		left = append(left, elem)                         // add NM to nonterminal
 		left = append(left, strconv.Itoa(ptrToRightPart)) // add ptr to right part of rule
 		ptrToRightPart += len(right)
+
 		for i := 0; i < len(right); i++ {
 			//add NM to nonterminal symbols
 			if len(right[i]) < 2 {
 				right[i] = append(right[i], elem)
 			}
+
 			if right[i][0] == grammary.EmptySymbol {
 				right[i] = append(right[i], "nullptr") // add empty ptr field for empty symbol
 			}
+
 			// fill ptr to next string int right parts
 			if !(grammary.IsNonTerminalSymbol(right[i][0])) {
 				if i == (len(right) - 1) {
@@ -89,6 +117,7 @@ func CreateTables(data string) ([][]string, [][]string) {
 				} else {
 					right[i] = append(right[i], strconv.Itoa(len(rightParts)+i+1))
 				}
+
 				// fill shift and stack for therminals
 				// shift
 				if right[i][0] != grammary.EmptySymbol {
@@ -97,58 +126,75 @@ func CreateTables(data string) ([][]string, [][]string) {
 					right[i] = append(right[i], "0")
 				}
 				right[i] = append(right[i], "0") // stack
+
 			} else {
-				right[i] = append(right[i], "")  // empty string for ptr to left part of rule
+				right[i] = append(right[i], "") // empty string for ptr to left part of rule
+
 				right[i] = append(right[i], "0") // No shift on nonterminal symbols
+
 				// fill stack field if needed
 				if i != (len(right) - 1) {
-					right[i] = append(right[i], strconv.Itoa(len(rightParts)+i))
+					right[i] = append(right[i], strconv.Itoa(len(rightParts)+1))
 				} else {
 					right[i] = append(right[i], "0")
 				}
 			}
+
 			rightParts = append(rightParts, right[i])
 		}
+
 		leftParts = append(leftParts, left)
+
 		creatingNM = false
 	}
+
 	// fill error column of left parts
 	for i := 0; i < len(leftParts); i++ {
+
 		if i != len(leftParts)-1 {
+
 			if leftParts[i][0] == leftParts[i+1][0] {
 				leftParts[i] = append(leftParts[i], strconv.Itoa(0))
 			} else {
 				leftParts[i] = append(leftParts[i], strconv.Itoa(1))
 			}
+
 		} else {
 			leftParts[i] = append(leftParts[i], strconv.Itoa(1))
 		}
 	}
+
 	// add ptr to nonterminal symbols and nullptr to EmptySymbol in right parts
 	for i := 0; i < len(rightParts); i++ {
 		v, ok := m[rightParts[i][0]]
+
 		if ok {
 			rightParts[i][2] = strconv.Itoa(v) // change empty ptr field
 		}
 	}
+
 	// correct NM in right parts
 	for i := 0; i < len(rightParts); i++ {
 		if grammary.IsNonTerminalSymbol(rightParts[i][0]) {
 			symbol := rightParts[i][0]
 			newNM := ""
 			ptr, err := strconv.Atoi(rightParts[i][2])
+
 			if err == nil {
 				newNM += leftParts[ptr][1]
 				ptr += 1
+
 				for ptr < len(leftParts) {
 					if leftParts[ptr][0] == symbol {
-						newNM += "," + leftParts[ptr][1]
+						newNM += "&" + leftParts[ptr][1]
 					}
 					ptr += 1
 				}
 			}
+
 			rightParts[i][1] = newNM
 		}
 	}
+
 	return leftParts, rightParts
 }
--- pkg/runner/app/runner.go
+++ pkg/runner/app/runner.go
@@ -8,45 +8,51 @@ import (
 	"strings"
 )
 
-func Runner(leftParts, rightParts [][]string, lexer lexer.Lexer) (bool, error) {
-	word, err := lexer.FetchLexem()
+type AnalyzerErr struct {
+	lexem    lexer.Lexem
+	expected string
+}
+
+func (err AnalyzerErr) Error() string {
+	return fmt.Sprintf("Error in %d:%d expected %s got %s", err.lexem.Line, err.lexem.Position, err.expected, err.lexem.Type)
+}
+
+func Runner(leftParts, rightParts [][]string, lexerImpl lexer.Lexer) error {
+	word, err := lexerImpl.FetchLexem()
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	symbol := leftParts[0][0]
 	var ptrToRight, ptrToLeft = 0, 0
 	var stack []int
 
-	for word.Value != "eof" {
-		fmt.Println("INF", word.Value)
+	for word.Type != "eof" {
 		for i := ptrToLeft; i < len(leftParts); i++ {
 			if symbol == leftParts[i][0] {
-				if strings.Contains(leftParts[i][1], word.Value) {
+				if strings.Contains(leftParts[i][1], string(word.Type)) {
 					ptrToRight, err = strconv.Atoi(leftParts[i][2])
 					if err != nil {
-						return false, err
+						return err
 					}
 					break
-				} else if leftParts[i][3] == strconv.Itoa(1) {
-					return false, nil
+				} else if leftParts[i][3] == "1" {
+					return err
 				}
 			}
 		}
 
-		// fmt.Println("Left", symbol, input[wordPos], ptrToLeft, ptrToRight, stack)
-
 		for i := ptrToRight; i < len(rightParts); i++ {
-			if strings.Contains(rightParts[i][1], word.Value) {
+			if strings.Contains(rightParts[i][1], string(word.Type)) {
 
 				shift, err2 := strconv.Atoi(rightParts[i][3])
 				if err2 != nil {
-					return false, err2
+					return err2
 				}
 				if shift == 1 {
-					word, err = lexer.FetchLexem()
+					word, err = lexerImpl.FetchLexem()
 					if err != nil {
-						return false, err
+						return err
 					}
 				}
 
@@ -59,7 +65,6 @@ func Runner(leftParts, rightParts [][]string, lexer lexer.Lexer) (bool, error) {
 				}
 
 				if rightParts[i][2] == "nullptr" && !grammary.IsNonTerminalSymbol(rightParts[i][0]) {
-					// fmt.Println("symbol", rightParts[i][0], stack)
 					if len(stack) > 0 {
 						top := len(stack) - 1
 						symbol = rightParts[stack[top]][0]
@@ -67,39 +72,47 @@ func Runner(leftParts, rightParts [][]string, lexer lexer.Lexer) (bool, error) {
 						stack = stack[:top]
 						continue
 					} else {
-						return checkEnd(stack, word.Value), nil
+						return checkEnd(stack, word, rightParts[i][1])
 					}
 				}
 
 				if grammary.IsNonTerminalSymbol(rightParts[i][0]) {
 					ptrToLeft, err = strconv.Atoi(rightParts[i][2])
 					if err != nil {
-						return false, err
+						return err
 					}
 					symbol = rightParts[i][0]
 
 					stackPtr, err2 := strconv.Atoi(rightParts[i][4])
 					if err2 != nil {
-						return false, err2
+						return err
 					}
 
 					if stackPtr > 0 {
 						stack = append(stack, stackPtr)
 					}
 					break
-				} else {
-					fmt.Println("symbol", rightParts[i][0])
 				}
 
 			} else {
-				return false, nil
+				return AnalyzerErr{
+					lexem:    word,
+					expected: rightParts[i][1],
+				}
 			}
 		}
 	}
 
-	return checkEnd(stack, word.Value), nil
+	return checkEnd(stack, word, string(lexer.LexemTypeEOF))
 }
 
-func checkEnd(stack []int, lexem string) (answer bool) {
-	return len(stack) == 0 && lexem == "eof"
+func checkEnd(stack []int, lexem lexer.Lexem, expectedLexem string) error {
+	if len(stack) == 0 && lexem.Type == "eof" {
+		return nil
+	}
+
+	return AnalyzerErr{
+		lexem:    lexem,
+		expected: expectedLexem,
+	}
 }
